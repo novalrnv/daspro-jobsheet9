40815e815f04efd83c99d5a54f6c1f215b7ab9da
novalrnv <akuunoval@gmail.com>
2024-10-31 15:12:35 +0700

    modification3

--- SearchNilai12.java
+++ SearchNilai12.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
+
 public class SearchNilai12 {
     public static void main(String[] args) {
-        int[] arrnilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+        int key;
         int hasil = 0;
+        int jmlhnilai;
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        jmlhnilai = sc.nextInt();
+        sc.nextLine();
+        
+        int[] arrnilai = new int[jmlhnilai];
+        for (int j = 0; j < arrnilai.length; j++){
+            System.out.print("Masukkan nilai Mahasiswa ke-" + (j + 1) + " : ");
+            arrnilai[j]= sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrnilai.length; i++){
-            if (key == arrnilai[i]){
+            if (arrnilai[i] == key){
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+
+        if (hasil != 0){
+            System.out.println();
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai Mahasiswa ke-"+(hasil+1));
+        } else {
+            System.out.println();
+            System.out.println("Nilai yang dicari tidak ditemukan!");
+        }
     }
 }
