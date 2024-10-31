dcb65b4bb7cac85ced0d5a0c6d236b0826e52fc4
novalrnv <akuunoval@gmail.com>
2024-10-31 11:22:30 +0700

    modification

--- ArrayBilangan12.java
+++ ArrayBilangan12.java
@@ -3,9 +3,9 @@ public class ArrayBilangan12{
         int[] bil = new int[4];
 
         bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+//         bil[1] = 13;
+//         bil[2] = -7;
+//         bil[3] = 17;
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
