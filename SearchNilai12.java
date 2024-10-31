bdc49f8295552146af4cd87118cbcac715f91900
novalrnv <akuunoval@gmail.com>
2024-10-31 14:27:06 +0700

    p3

---
+++ SearchNilai12.java
@@ -0,0 +1,16 @@
+public class SearchNilai12 {
+    public static void main(String[] args) {
+        int[] arrnilai = {80, 85, 78, 96, 90, 82, 86};
+        int key = 90;
+        int hasil = 0;
+
+        for (int i = 0; i < arrnilai.length; i++){
+            if (key == arrnilai[i]){
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+    }
+}
